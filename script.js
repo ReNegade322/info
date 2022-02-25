00ff992d2a3c8544935ee7734811d9eb5d0495a1
ReNegade <evis2k@mail.ru>
2022-02-25 22:23:06 +0300

    loggin brauser info

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hi")
+document.write('Информация браузере: '+ navigator.userAgent + '<br />');
+
+
+if (navigator.cookieEnabled)
+   document.write('Браузер может принимать cookie!' + '<br />');
+else
+   document.write(' Ваш браузер не может принимать cookie.' + '<br />');
+
+document.write("Браузер: " + navigator.appCodeName + '<br />');
+
+document.write("Платформа, под которую скомпилирован браузер: " + navigator.platform + '<br />');
+
+document.write("Включена ли в браузере поддержка Java: " + navigator.javaEnabled() + '<br />');
